a047c4dbf9241f14fe9435c67fc109d5a0ab602e
Davide Scola <davide@bitfinex.com>
2018-01-22 16:03:17 +0000

    copyright update

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 ############################################################################
 # This file is part of Grenache Command Line Interface.                    #
 #                                                                          #
-# Copyright (C) 2017 Davide Scola <davide@bitfinex.com>                    #
+# Copyright (C) 2017, 2018 Davide Scola <davide@bitfinex.com>              #
 #                                                                          #
 # Licensed under the Apache License,  Version 2.0 (the "License"); you may #
 # not use this file except in  compliance with the License. You may obtain #
